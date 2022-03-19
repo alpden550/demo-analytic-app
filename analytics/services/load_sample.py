067a8d1aac2a6d6fc9d74114a22d5c3cfb7b9e49
Denis Novikov <alpden550@gmail.com>
2022-03-19 10:51:34 +0300

    feat(services): add bulk create for sample insights

--- analytics/services/load_sample.py
+++ analytics/services/load_sample.py
@@ -4,7 +4,11 @@ from analytics.models import Insight
 
 
 def load_sample_csv_dataset(path: str = "analytics/samples/dataset.csv") -> None:
+    insights = []
+
     with open(path) as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
-            Insight.objects.create(**row)
+            insights.append(Insight(**row))
+
+    Insight.objects.bulk_create(insights)
